7c21cdd7bea6a3030781bce5e490f8423636b114
Yannik Ehlert <yannik.ehlert@claas.com>
2020-12-09 19:23:06 +0100

    SendBirdSyncManager v1.1.34

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "SendBirdSyncManager", url: "https://github.com", checksum: "0000000000000000000000000000000000000000000000000000000000000000"),
+        .binaryTarget(name: "SendBirdSyncManager", url: "https://github.com/yanniks/sendbird-syncmanager-ios/releases/download/v1.1.34/SendBirdSyncManager.zip", checksum: "f5017281e511c8c84358a75c9b440c3be2a03a99e26f64d07e654bf4acb264b1"),
     ]
 )
